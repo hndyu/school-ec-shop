ca1c0285edcaa8c5c3f7f12c692211bf887ab593
hndyu <4225495+hndyu@users.noreply.github.com>
2025-10-03 11:07:11 +0900

    first commit

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("http://localhost:3000/api/products");
+      const data = await response.json();
+      // console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <ProductContext.Provider value={{ products }}>
+        {children}
+      </ProductContext.Provider>
+    </div>
+  );
+};
+
+export default ProductProvider;
